1490262b332b182b39944919c2cad8b570fc4af9
cloud-jake <jake.holmquist@kinandcarta.com>
2023-04-18 22:10:05 -0400

    DRI027 initial push

--- Database Migration and Modernization/DRI027/05-implement-fallback.sh
+++ Database Migration and Modernization/DRI027/05-implement-fallback.sh
@@ -23,31 +23,30 @@ source variables.inc
 # Task 5. Implement a fallback
 
 ## 1 - Create HA cross-region replica
-gcloud sql instances create ${REPLICA3} \
+# create new read-replica
+gcloud sql instances create ${REPLICA2} \
     --master-instance-name=${REPLICA1} \
-    --region=${REGION1} \
-    --availability-type="REGIONAL" \
-    --enable-bin-log
+    --region=${REGION3} \
+    --availability-type="REGIONAL" 
 
-## 2 - Promote replica3 as new primary
+
+## 2 - Promote replica2 as new primary
 ###First stop the primary and stop replication????????
 
-gcloud sql instances promote-replica ${REPLICA3}
+gcloud sql instances promote-replica ${REPLICA2}
 
 
 
 ## 3 - Delete Replica1 and Replica 2
 ### first stop them
 gcloud sql instances patch ${REPLICA1} --activation-policy NEVER
-gcloud sql instances patch ${REPLICA2} --activation-policy NEVER
 
 ### then delete them
 gcloud sql instances delete ${REPLICA1}
-gcloud sql instances delete ${REPLICA2}
 
 
 ## 4 - create new read replica
-gcloud sql instances create ${REPLICA4} \
-    --master-instance-name=${REPLICA3} \
+gcloud sql instances create ${REPLICA3} \
+    --master-instance-name=${REPLICA2} \
     --region=${REGION2} \
     --availability-type="REGIONAL" 
