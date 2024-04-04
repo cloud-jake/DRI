a0f8865106066c8c6f5d675cd77a4b679d8062d7
cloud-jake <jake.holmquist@kinandcarta.com>
2024-04-03 20:25:23 -0400

    Gen AI add....

---
+++ Generative AI/Orchestrating LLMs with LangChain: Challenge Lab/task-01.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Run the following commands from a Jupyter Notebook Terminal window:
+
+gsutil cp gs://partner-usecase-bucket/genai028/orchestration_llms_with_langchain_challenge_notebook.ipynb .
+
